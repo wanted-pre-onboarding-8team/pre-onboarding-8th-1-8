972b64d99cd2404808497ec3253e7a3843b2a1c7
minkyung00 <mksin00@naver.com>
2022-12-22 21:42:41 +0900

    refactor: useInput handleChange 조건 처리 수정

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -6,11 +6,9 @@ const useInput = ({ initialValue, errorMessage, required, type }) => {
   const [valid, setValid] = useState(true);
   const regexp = VALIDATIONS[type];
 
-  const handleChange = event => {
-    if(event.target.value.length !== 0){
-      event.target.value.match(regexp) ? setValid(() => true) : setValid(() => false);
-    }
-    setValue(() => event.target.value);
+  const handleChange = ({ target: { value } }) => {
+    setValid(!!value.match(regexp));
+    setValue(value);
   };
 
   return {
