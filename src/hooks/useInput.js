894794dfee80add7a4e4ed6c8e619ecd34aada57
dev-dong-su <fkdlrj1230@gmail.com>
2022-12-22 20:22:37 +0900

    refactor: useInput pattern -> type, LocalStorage Value -> storageValue

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,10 +1,10 @@
 import VALIDATIONS from 'constants/Validations';
 import { useState } from 'react';
 
-const useInput = ({ initialValue, errorMessage, required, pattern }) => {
+const useInput = ({ initialValue, errorMessage, required, type }) => {
   const [value, setValue] = useState(initialValue);
   const [valid, setValid] = useState(false);
-  const regexp = VALIDATIONS[pattern];
+  const regexp = VALIDATIONS[type];
 
   const handleChange = event => {
     event.target.value.match(regexp) ? setValid(() => true) : setValid(() => false);
--- src/router/Router.js
+++ src/router/Router.js
@@ -5,9 +5,9 @@ import AccountPage from '../pages/AccountPage';
 import TmpPage from '../pages/TMP';
 
 export const PrivateRouter = () => {
-  const { storedValue } = useLocalStorage('access_token');
+  const { storageValue } = useLocalStorage('access_token');
 
-  return storedValue === undefined || storedValue === null ? <Navigate to="/" /> : <TmpPage />;
+  return storageValue === undefined || storageValue === null ? <Navigate to="/" /> : <TmpPage />;
 };
 
 export const router = createBrowserRouter([
