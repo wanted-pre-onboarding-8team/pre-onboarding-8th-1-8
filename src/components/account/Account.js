1a74faad9ba1e46351bd3f5a927fb481ecd924d2
dev-dong-su <fkdlrj1230@gmail.com>
2022-12-22 17:37:26 +0900

    refactor: Container 컴포넌트 제거 common에 이름을 Styled~로 수정

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -1,10 +1,11 @@
 import tw from 'tailwind-styled-components';
 import useAuthMethod from '../../hooks/useAuthMethod';
-import Container from '../common/Container';
 import Radio from '../common/Radio';
 import RadioGroup from '../RadioGroup';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
+
+const Container = tw.div`flex flex-col shadow-md items-center w-11/12 h-5/6 mx-auto rounded-xl gap-2 bg-slate-50 my-auto`;
 
 const Header = tw.div`flex flex-col items-center gap-5 w-full shadow-md p-4 rounded-xl`;
 
--- src/components/common/PageTemplate.js
+++ src/components/common/PageTemplate.js
@@ -1,9 +1,9 @@
 import tw from 'tailwind-styled-components';
 
-const Container = tw.div`flex container mx-auto max-w-screen-md h-screen my-auto`;
+const StyledContainer = tw.div`flex container mx-auto max-w-screen-md h-screen my-auto`;
 
 const PageTemplate = ({ children, ...rest }) => {
-  return <Container {...rest}>{children}</Container>;
+  return <StyledContainer {...rest}>{children}</StyledContainer>;
 };
 
 export default PageTemplate;
