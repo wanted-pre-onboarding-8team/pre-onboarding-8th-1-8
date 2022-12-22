4b711e6601575b9d2d4067ed6d79c8c432193b35
dev-dong-su <fkdlrj1230@gmail.com>
2022-12-22 20:21:39 +0900

    refactor: Input Error 메시지 상수화

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -2,8 +2,8 @@ import tw from 'tailwind-styled-components';
 
 import RadioGroup from '../RadioGroup';
 import Radio from '../common/Radio';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
+import SignIn from './SignUp';
+import SignUp from './SignIn';
 import useAccountMethod from '../../hooks/useAccountMethod';
 
 const Container = tw.div`flex flex-col shadow-md items-center w-11/12 h-5/6 mx-auto rounded-xl gap-2 bg-slate-50 my-auto`;
@@ -13,18 +13,18 @@ const Header = tw.div`flex flex-col items-center gap-5 w-full shadow-md p-4 roun
 const Title = tw.h2`text-7xl text-rose-400`;
 
 const Account = () => {
-  const { accountMethod, setAccountMethod } = useAccountMethod('login');
+  const { accountMethod, setAccountMethod } = useAccountMethod('signin');
 
   return (
     <Container>
       <Header>
         <Title>TODO</Title>
         <RadioGroup value={accountMethod} onChange={setAccountMethod}>
-          <Radio value="login">Login</Radio>
-          <Radio value="join">Join</Radio>
+          <Radio value="signin">로그인</Radio>
+          <Radio value="signup">회원가입</Radio>
         </RadioGroup>
       </Header>
-      {accountMethod === 'login' ? <SignUp /> : <SignIn />}
+      {accountMethod === 'signin' ? <SignUp /> : <SignIn />}
     </Container>
   );
 };
