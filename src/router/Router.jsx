adb141a63796ce06741c57d0000532b309c0493e
leezer94 <2kunhee94@gmail.com>
2022-12-23 19:27:04 +0900

    refactor: PrivateRouter / PublicRouter 로직 수정및 적용

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,21 +2,44 @@ import { LOCAL_STORAGE, ROUTES } from 'constants';
 import { useLocalStorage } from 'hooks';
 import AccountPage from 'pages/AccountPage';
 import TodoPage from 'pages/ToDoPage';
-import { Navigate, createBrowserRouter } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Navigate, Outlet, createBrowserRouter, useLocation } from 'react-router-dom';
 
-export const PrivateRouter = () => {
-  const { storageValue } = useLocalStorage(LOCAL_STORAGE.ACCESS_TOKEN);
+const PrivateRouter = () => {
+  const { storageValue: accessToken } = useLocalStorage(LOCAL_STORAGE.ACCESS_TOKEN);
 
-  return storageValue === undefined || storageValue === null ? <Navigate to="/" /> : <TodoPage />;
+  return !accessToken ? <Navigate to={ROUTES.HOME} replace /> : <Outlet />;
+};
+
+useEffect;
+const PublicRouter = () => {
+  const { pathname } = useLocation();
+  const { storageValue: accessToken } = useLocalStorage(LOCAL_STORAGE.ACCESS_TOKEN);
+
+  if (pathname === ROUTES.HOME) {
+    return accessToken ? <Navigate to={ROUTES.TODO} replace /> : <Outlet />;
+  }
+
+  return accessToken ? <Navigate to={ROUTES.HOME} replace /> : <Outlet />;
 };
 
 export const router = createBrowserRouter([
   {
-    path: ROUTES.HOME,
-    element: <AccountPage />,
+    element: <PublicRouter />,
+    children: [
+      {
+        path: ROUTES.HOME,
+        element: <AccountPage />,
+      },
+    ],
   },
   {
-    path: ROUTES.TODO,
     element: <PrivateRouter />,
+    children: [
+      {
+        path: ROUTES.TODO,
+        element: <TodoPage />,
+      },
+    ],
   },
 ]);
