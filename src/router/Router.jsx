1294b55e79dc10eca70d2ec48610d80a7d8e6b11
leezer94 <2kunhee94@gmail.com>
2022-12-23 22:28:09 +0900

    feat: suspense 및 lazy import 추가

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,12 @@
 import { ROUTES } from 'constants';
-import AccountPage from 'pages/AccountPage';
-import TodoPage from 'pages/ToDoPage';
+import { Suspense, lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
 import PrivateRouter from './PrivateRouter';
 import PublicRouter from './PublicRouter';
+
+const AccountPage = lazy(() => import('pages/AccountPage'));
+const TodoPage = lazy(() => import('pages/ToDoPage'));
 
 export const router = createBrowserRouter([
   {
@@ -12,7 +14,11 @@ export const router = createBrowserRouter([
     children: [
       {
         path: ROUTES.HOME,
-        element: <AccountPage />,
+        element: (
+          <Suspense fallback={<div>loading...</div>}>
+            <AccountPage />
+          </Suspense>
+        ),
       },
     ],
   },
@@ -21,7 +27,11 @@ export const router = createBrowserRouter([
     children: [
       {
         path: ROUTES.TODO,
-        element: <TodoPage />,
+        element: (
+          <Suspense fallback={<div>loading...</div>}>
+            <TodoPage />
+          </Suspense>
+        ),
       },
     ],
   },
